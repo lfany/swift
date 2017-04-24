d2faf81e4a37a6b65a3d17d280f03ebce6dc214e
hello <hello@ifnot.cc>
2017-04-24 07:45:14 +0000

    scriptrewrite

---
+++ basic/scripteriter/Sources/main.swift
@@ -0,0 +1,21 @@
+/* Writes and executes a script in Swift that echos
+   the arguments passed in. Shows how to execute scripts
+   in Swift and how to read arguments.
+*/
+
+#if os(OSX) || os(iOS) || os(watchOS) || os(tvOS)
+    import Darwin
+#else
+    import Glibc
+#endif
+
+system("touch script.swift")
+let stream = fopen("script.swift", "r+")
+let program = "#!/usr/bin/swift \n print(\"CommandLine.arguments gave args:\")\n" +
+    "for s in CommandLine.arguments {print(s)}"
+fwrite(program, 1, program.characters.count, stream)
+
+fclose(stream)
+system("chmod +x script.swift")
+system("./script.swift -- first second")
+
---
+++ basic/scripteriter/script.swift
@@ -0,0 +1,3 @@
+#!/usr/bin/swift 
+ print("CommandLine.arguments gave args:")
+for s in CommandLine.arguments {print(s)}
